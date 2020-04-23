4beb4d17917a8ab45b23ed4b28c5674349023d21
Andrey Opanasenko <aopanasenko@dclab.local>
2020-04-23 13:49:47 +0300

    Finished Facade pattern

---
+++ src/structural/Facade/Facade.ts
@@ -0,0 +1,42 @@
+/**
+ * Фасад
+ */
+
+/**
+ * Пусть есть классы со сложной стурктурой, которые мы не можем упростить
+ */
+class VideoFile {};
+
+class OggCompressionCodec {};
+
+class MPEG4CompressionCoded {};
+
+class AudioMixer {
+    public fix(result: any) {
+        return result;
+    }
+};
+
+class FileClass {
+    private newFile: any;
+
+    constructor(newFile: any) {
+        this.newFile = newFile;
+    }
+};
+
+// Создадим фасад
+class VideoConverter {
+    public convert(filename: string, format: string): FileClass {
+        const file = new VideoFile();
+
+        if (format === 'mp4') {
+            const destinationCodec = new MPEG4CompressionCoded();
+        } else {
+            const destinationCodec = new OggCompressionCodec();
+        }
+
+        const result = (new AudioMixer()).fix(file);
+        return new FileClass(result);
+    }
+}
--- src/structural/index.ts
+++ src/structural/index.ts
@@ -2,3 +2,4 @@ export * from './Adapter';
 export * from './Bridge';
 export * from './Composite';
 export * from './Decorator';
+export * from './Facade';
