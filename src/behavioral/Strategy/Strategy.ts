35b32ce1e668b05a36dd328d44e020352796c199
aopanasenko <email@aopanasenko.ru>
2024-03-04 18:22:09 +0300

    DP-0 Рефакторинг паттерна Стратегия

--- src/behavioral/Strategy/Strategy.ts
+++ src/behavioral/Strategy/Strategy.ts
@@ -2,24 +2,25 @@
  * Стратегия.
  */
 
-interface Strategy {
-    buildRoute(A: string, B: string): void;
+
+export abstract class Strategy {
+    abstract buildRoute(A: string, B: string): void;
 }
 
 // Конкретные стратегии. Должны иметь единый интерфейс
-class RoadStrategy implements Strategy {
+class RoadStrategy extends Strategy {
     public buildRoute(A: string, B: string) {
         console.log(`Build route from ${A} to ${B} with RoadStrategy`);
     }
 }
 
-class WalkingStrategy implements Strategy {
+class WalkingStrategy extends Strategy {
     public buildRoute(A: string, B: string) {
         console.log(`Build route from ${A} to ${B} with WalkingStrategy`);
     }
 }
 
-class PublicTransportStrategy implements Strategy {
+class PublicTransportStrategy extends Strategy {
     public buildRoute(A: string, B: string) {
         console.log(`Build route from ${A} to ${B} with PublicTransportStrategy`);
     }
