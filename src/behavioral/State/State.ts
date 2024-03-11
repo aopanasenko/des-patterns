b6703b98a93118f9e3880061150c4c7937bf5736
Andrey Opanasenko <35809906+aopanasenko@users.noreply.github.com>
2024-03-11 23:39:08 +0300

    DP-0 Рефакторинг (#2)
    * DP-0 Переосмысление
    
    * DP-0 Рефакторинг паттерна Декоратор
    
    * DP-0 Рефакторинг паттерна Декоратор
    
    * DP-0 Рефакторинг паттерна Декоратор
    
    * DP-0 Рефакторинг паттерна Декоратор
    
    * DP-0 Рефакторинг паттерна Шаблонный метод
    
    * DP-0 Рефакторинг паттерна Шаблонный метод
    
    * DP-0 Рефакторинг паттерна Стратегия
    
    * DP-0 Поправил скрипт компиляции
    
    * DP-0 Исправлен паттерн Состояние
    
    * DP-0 Исправлен паттерн Состояние
    
    * DP-0 Исправлен паттерн Состояние
    
    * DP-0 Исправлен паттерн Состояние

--- src/behavioral/State/State.ts
+++ src/behavioral/State/State.ts
@@ -2,40 +2,95 @@
  * Состояние.
  */
 
-class StepStatus {
+
+/**
+ * Реализация при которой каждое состояние знает о том, в какое следующее состояние переходить.
+ * Инициатор смены состояния - контекст и состояние.
+ * Отличия от паттерна "Стратегия" в том, что контекст и состояния знают друг о друге.
+ */
+abstract class State {
     private name: string;
-    private nextStep: object;
+    private context: IOrder | null = null;
 
-    constructor(name: string, nextStep: object) {
+    constructor(name: string) {
         this.name = name;
-        this.nextStep = nextStep;
     }
 
-    public next() {
-        return this.nextStep();
+    public setContext(context: IOrder): void {
+        this.context = context;
+    }
+
+    /**
+     * Реализация логики в зависимости от состояния.
+     */
+    public purchase(): string {
+        return `Purchase from state: ${this.name}`;
+    }
+
+    /**
+     * Каждое состояние может само инициировать смену состояния.
+     */
+    private setState(newState: State) {
+        if (this.context) {
+            this.context.setState(newState);
+        }
     }
 }
 
-class Payment extends StepStatus {
+// Каждое состояние само знает какое состояние будет следующим
+class PaymentState extends State {
     constructor() {
-        super('Payment', Delivery);
+        super('Payment');
     }
 }
 
-class Delivery extends StepStatus {
+class DeliveryState extends State {
     constructor() {
-        super('Delivery', Delivery);
+        super('Delivery');
     }
 }
 
-export class Order {
-    private state: Payment | Delivery;
-
+class HandingState extends State {
     constructor() {
-        this.state = new Payment();
-    }
-
-    nextState() {
-        this.state = this.state.next();
+        super('Handing');
     }
 }
+
+export interface IOrder {
+    purchase(): string;
+    setState(state: State): void;
+}
+
+export class Order implements IOrder {
+    private state: State;
+
+    constructor(initialState: State) {
+        this.state = initialState;
+        this.state.setContext(this);
+    }
+
+    /**
+     * Контекст делегирует выполнение соответствующему состоянию.
+     */
+    public purchase(): string {
+        return this.state.purchase();
+    }
+
+    public setState(newState: State): void {
+        this.state = newState;
+    }
+}
+
+const paymentState = new PaymentState();
+const deliveryState = new DeliveryState();
+const handingState = new HandingState();
+
+const order = new Order(paymentState);
+// Purchase from state: Payment
+console.log(order.purchase());
+order.setState(deliveryState);
+// Purchase from state: Delivery
+console.log(order.purchase());
+order.setState(handingState);
+// Purchase from state: Handing
+console.log(order.purchase());
