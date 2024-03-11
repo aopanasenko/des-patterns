7c96a6d5a9d7d216eb295888234eb352f7c4d25e
aopanasenko <email@aopanasenko.ru>
2024-03-11 23:37:46 +0300

    DP-0 Исправлен паттерн Состояние

--- src/behavioral/State/State.ts
+++ src/behavioral/State/State.ts
@@ -5,56 +5,92 @@
 
 /**
  * Реализация при которой каждое состояние знает о том, в какое следующее состояние переходить.
- * Инициатор смены состояния - контекст. Он вызывает метод смены состояния из экземпляра State.
- * Состояния создаются по мере их необходимости и не хранятся в памяти все время.
+ * Инициатор смены состояния - контекст и состояние.
+ * Отличия от паттерна "Стратегия" в том, что контекст и состояния знают друг о друге.
  */
 abstract class State {
     private name: string;
-    private nextState: new () => State;
+    private context: IOrder | null = null;
 
-    constructor(name: string, nextState: new () => State) {
+    constructor(name: string) {
         this.name = name;
-        this.nextState = nextState;
     }
 
-    public getStateName(): string {
-        return this.name;
+    public setContext(context: IOrder): void {
+        this.context = context;
     }
 
-    public setNextState(): State {
-        return new this.nextState();
+    /**
+     * Реализация логики в зависимости от состояния.
+     */
+    public purchase(): string {
+        return `Purchase from state: ${this.name}`;
+    }
+
+    /**
+     * Каждое состояние может само инициировать смену состояния.
+     */
+    private setState(newState: State) {
+        if (this.context) {
+            this.context.setState(newState);
+        }
     }
 }
 
 // Каждое состояние само знает какое состояние будет следующим
 class PaymentState extends State {
     constructor() {
-        super('Payment', DeliveryState);
+        super('Payment');
     }
 }
 
 class DeliveryState extends State {
     constructor() {
-        super('Delivery', HandingState);
+        super('Delivery');
     }
 }
 
 class HandingState extends State {
     constructor() {
-        super('Handing', HandingState);
+        super('Handing');
     }
 }
 
-export class Order {
+export interface IOrder {
+    purchase(): string;
+    setState(state: State): void;
+}
+
+export class Order implements IOrder {
     private state: State;
 
-    constructor() {
-        this.state = new PaymentState();
+    constructor(initialState: State) {
+        this.state = initialState;
+        this.state.setContext(this);
     }
 
-    nextState() {
-        this.state = this.state.setNextState();
-        console.log(`Move to the next state: ${this.state.getStateName()}`);
+    /**
+     * Контекст делегирует выполнение соответствующему состоянию.
+     */
+    public purchase(): string {
+        return this.state.purchase();
+    }
+
+    public setState(newState: State): void {
+        this.state = newState;
     }
 }
 
+const paymentState = new PaymentState();
+const deliveryState = new DeliveryState();
+const handingState = new HandingState();
+
+const order = new Order(paymentState);
+// Purchase from state: Payment
+console.log(order.purchase());
+order.setState(deliveryState);
+// Purchase from state: Delivery
+console.log(order.purchase());
+order.setState(handingState);
+// Purchase from state: Handing
+console.log(order.purchase());
